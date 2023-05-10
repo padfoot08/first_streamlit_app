1433021b7819ced51905359b9e328c90bee8095c
Dean <69795424+padfoot08@users.noreply.github.com>
2023-05-10 14:50:29 +0800

    Update streamlit_app.py
    Added header and added texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('To Every You I have loved before')
+streamlit.header('Boku ga Aishita Subete no Kimi e')
+streamlit.text('Hio Miyazawa')
+streamlit.text('Ai Hashimoto')
+streamlit.text('Miki Mizuno')
+streamlit.text('Mutsumi Tamura')
