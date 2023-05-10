290ed1ae98c28cb6b7ec83c219a700e45ebf324f
Dean <69795424+padfoot08@users.noreply.github.com>
2023-05-10 14:56:53 +0800

    Update streamlit_app.py
    Added new headers for the smoothies and 3 new flavors

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,8 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('Build Your Own Fruit Smoothie')
+streamlit.text('🍌🥭 Strawberry Banana Milkshake')
+streamlit.text('🥝 Kiwi Milky Frost')
+streamlit.text('🍇 Grape Ooze Blended Drink')
